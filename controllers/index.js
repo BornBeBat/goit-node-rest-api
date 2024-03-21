81935528dcd03ee9b819d5acc9e2dff0152d54dc
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-03-21 14:01:48 +0200

    updateContastStatus

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,15 @@ import { getOneContact } from "./getOneContact.js";
 import { createContact } from "./createContact.js";
 import { updateContact } from "./updateContact.js";
 import { deleteContact } from "./deleteContact.js";
+import { updateContactStatus } from "./updateContactStatus.js";
 
-import { ctrlWrapper } from "../middlewares/ctrlWrapper.js";
+import { ctrlWrapper } from "../decorators/ctrlWrapper.js";
 
 export default {
   getAllContacts: ctrlWrapper(getAllContacts),
   getOneContact: ctrlWrapper(getOneContact),
   createContact: ctrlWrapper(createContact),
   updateContact: ctrlWrapper(updateContact),
+  updateContactStatus: ctrlWrapper(updateContactStatus),
   deleteContact: ctrlWrapper(deleteContact),
 };
