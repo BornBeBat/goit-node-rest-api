fee45876e4d45087209e99b7959d9be626b91460
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-03-21 14:02:50 +0200

    revork servises

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,57 +1,38 @@
-import fs from "fs/promises";
-import path from "path";
-import { randomUUID } from "crypto";
-
-const contactsPath = path.join("..", "db", "contacts.json");
+import { Contact } from "../models/Contact.js";
 
 export async function listContacts() {
-  const dataString = await fs.readFile(contactsPath, "utf8");
-  const data = JSON.parse(dataString);
+  const data = await Contact.find({});
   return data;
 }
 
 export async function getContactById(contactId) {
-  const contactsList = await listContacts();
-  const contact = contactsList.find((item) => item.id === contactId);
-  return contact ? contact : null;
+  try {
+    const contact = await Contact.findById(contactId);
+    return contact;
+  } catch (error) {
+    return null;
+  }
 }
 
-export async function addContact({ name, email, phone }) {
-  const contactsList = await listContacts();
-  const newContact = {
-    id: randomUUID(),
-    name,
-    email,
-    phone,
-  };
-  contactsList.push(newContact);
-
-  fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
+export async function addContact(body) {
+  const newContact = await Contact.create(body);
   return newContact;
 }
 
 export async function updateContactServises(id, data) {
-  const contactsList = await listContacts();
-  const idx = contactsList.findIndex((item) => item.id === id);
-
-  if (idx === -1) return null;
-  console.log();
-  contactsList[idx] = {
-    ...contactsList[idx],
-    ...data,
-  };
-
-  fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
-  return contactsList[idx];
+  try {
+    const contact = await Contact.findByIdAndUpdate(id, data, { new: true });
+    return contact;
+  } catch (error) {
+    return null;
+  }
 }
 
 export async function removeContact(contactId) {
-  const contactsList = await listContacts();
-  const idx = contactsList.findIndex((contact) => contact.id === contactId);
-
-  if (idx === -1) return null;
-
-  const deletedContact = contactsList.splice(idx, 1);
-  fs.writeFile(contactsPath, JSON.stringify(contactsList, null, 2));
-  return deletedContact[0];
+  try {
+    const result = await Contact.findByIdAndDelete(contactId);
+    return result;
+  } catch (error) {
+    return null;
+  }
 }
