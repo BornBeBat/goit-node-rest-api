53cb7251ea788e7a3c210c5733d40b31fe71d086
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-03-27 11:37:22 +0200

    users joi an mongose shemas

--- models/Users.js
+++ models/Users.js
@@ -1,15 +1,17 @@
 import { Shema, model } from "mongoose";
 
-import { subscriptionList } from "../constant";
+import { emailRegExp, subscriptionList } from "../constant";
 
 const usersShema = new Shema({
   password: {
     type: String,
+    minLength: 6,
     required: [true, "Password is required"],
   },
   email: {
     type: String,
     required: [true, "Email is required"],
+    match: emailRegExp,
     unique: true,
   },
   subscription: {
---
+++ schemas/usersShemas.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+import { emailRegExp, subscriptionList } from "../constant";
+
+export const addUserShema = Joi.object({
+  password: Joi.string().required().min(6),
+  email: Joi.string().required().pattern(emailRegExp),
+  subscription: Joi.string().valid(...subscriptionList),
+});
